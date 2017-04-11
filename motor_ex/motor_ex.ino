9622565b3553e420506b557ad3439c81b82d7c79
Курушин Даниил <daniel.kurushin@gmail.com>
2017-04-11 17:13:14 +0500

    no move

--- motor_ex/motor_ex.ino
+++ motor_ex/motor_ex.ino
@@ -31,69 +31,63 @@ void setup() {
     delta += new_z - old_z;
   }
   delta /= 10;
-
-  pinMode(M1AIN,OUTPUT);
-  pinMode(M1BIN,OUTPUT);
-  pinMode(M1PWM,OUTPUT);
+ 
+  pinMode(M1AIN, OUTPUT);
+  pinMode(M1BIN, OUTPUT);
+  pinMode(M1PWM, OUTPUT);
   pinMode(M1AEN, OUTPUT);
   pinMode(M1BEN, OUTPUT);
 
-  pinMode(M2AIN,OUTPUT);
-  pinMode(M2BIN,OUTPUT);
-  pinMode(M2PWM,OUTPUT);
+  pinMode(M2AIN, OUTPUT);
+  pinMode(M2BIN, OUTPUT);
+  pinMode(M2PWM, OUTPUT);
   pinMode(M2AEN, OUTPUT);
   pinMode(M2BEN, OUTPUT);
 }
 
 int lVal = 0;
-//float lValPure = 0.0;
 int rVal = 0;
-//float rValPure = 0.0;
+
+//void nothing()
+//}
+//  lVal = 256;
+//  if (lVal > 0) {
+//    digitalWrite(M1AEN, HIGH);
+//    digitalWrite(M1AIN, HIGH);
+//    digitalWrite(M1BIN, LOW);
+//  } else if (lVal < 0) {
+//    digitalWrite(M1BEN, HIGH);
+//    digitalWrite(M1AIN, LOW);
+//    digitalWrite(M1BIN, HIGH);
+//  } else if (lVal == 0) {
+//    digitalWrite(M1AEN, LOW);
+//    digitalWrite(M1BEN, LOW);
+//  }
+//
+//
+//  analogWrite(M1PWM, (int) abs(lVal/2));
+//
+//  rVal = 256;
+//  if (rVal > 0) {
+//    digitalWrite(M2AEN, HIGH);
+//    digitalWrite(M2AIN, HIGH);
+//    digitalWrite(M2BIN, LOW);
+//  } else if (rVal < 0) {
+//    digitalWrite(M2BEN, HIGH);
+//    digitalWrite(M2AIN, LOW);
+//    digitalWrite(M2BIN, HIGH);
+//  } else if (rVal == 0) {
+//    digitalWrite(M2AEN, LOW);
+//    digitalWrite(M2BEN, LOW);
+//  }
+//}
+
 void loop() {
-  pure_gz=GY85.gyro_z( GY85.readGyro() ) * dt;
-  gz += pure_gz - delta*dt*0.6;
+  pure_gz = GY85.gyro_z( GY85.readGyro() ) * dt;
+  gz += pure_gz - delta * dt;
 
-//  lValPure = 0;
-//  for (int i = 0; i < 8; i++) {
-//    lValPure += analogRead(A0);
-//  }
-//  lVal = (int)(lValPure / 8) - 512;
-//  Serial.println(lVal);
-  lVal = 256;
-  if (lVal > 0) {
-    digitalWrite(M1AEN, HIGH);
-    digitalWrite(M1AIN, HIGH);
-    digitalWrite(M1BIN, LOW);
-  } else if (lVal < 0) {
-    digitalWrite(M1BEN, HIGH);
-    digitalWrite(M1AIN, LOW);
-    digitalWrite(M1BIN, HIGH);
-  } else if (lVal == 0) {
-    digitalWrite(M1AEN, LOW);
-    digitalWrite(M1BEN, LOW);
-  }
-  analogWrite(M1PWM, (int) abs(lVal/2));
+  //analogWrite(M2PWM, (int) (abs(rVal/2) + gz));
 
-//  rValPure = 0;
-//  for (int i = 0; i < 8; i++) {
-//    rValPure += analogRead(A1);
-//  }
-//  rVal = (int)(rValPure / 8) - 512;
-//  Serial.println(rVal);
-  rVal = 256;
-  if (rVal > 0) {
-    digitalWrite(M2AEN, HIGH);
-    digitalWrite(M2AIN, HIGH);
-    digitalWrite(M2BIN, LOW);
-  } else if (rVal < 0) {
-    digitalWrite(M2BEN, HIGH);
-    digitalWrite(M2AIN, LOW);
-    digitalWrite(M2BIN, HIGH);
-  } else if (rVal == 0) {
-    digitalWrite(M2AEN, LOW);
-    digitalWrite(M2BEN, LOW);
-  }
-  analogWrite(M2PWM, (int) (abs(rVal/2) + gz));
   Serial.println(abs(lVal/2));
   Serial.println((int) (abs(rVal/2) + gz));
   Serial.println(gz);
