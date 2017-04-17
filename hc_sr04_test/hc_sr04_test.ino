ec3739376a9de81b98a0c25247a5723e0d796975
Курушин Даниил <daniel.kurushin@gmail.com>
2017-04-17 19:55:52 +0500

    HC-SR04

--- hc_sr04_test/hc_sr04_test.ino
+++ hc_sr04_test/hc_sr04_test.ino
@@ -1,119 +1,60 @@
-int echoPinA = 11;
+int echoPinA = 2;
 int trigPinA = 12;
 int echoPinB = 11;
 int trigPinB = 12;
-//            0      1     2     3      4      5
-typedef enum {
-  BEGIN, TRIG, WAIT, ECHOR, ECHOL, ERROR1, ERROR2} 
-tstt;
 
-volatile uint8_t  err = 0;
-volatile tstt     stt = BEGIN;
 volatile uint16_t tme = 0;
-volatile uint16_t dur = 0;
-
+volatile float dur = 0;
 
 void setup()
 {
-  pinMode(8, OUTPUT);
+	pinMode(8, OUTPUT);
+	Serial.begin (115200);
+	pinMode(trigPinA, OUTPUT);
+	pinMode(echoPinA, INPUT);
+	pinMode(trigPinB, OUTPUT);
+	pinMode(echoPinB, INPUT);
 
-  Serial.begin (115200);
-  pinMode(trigPinA, OUTPUT);
-  pinMode(echoPinA, INPUT);
-  pinMode(trigPinB, OUTPUT);
-  pinMode(echoPinB, INPUT);
-
-  // attachInterrupt(0, right_t, RISING);
-  // attachInterrupt(1, left_t, RISING);
-
-  OCR0A = 0xFF;
-  TIMSK0 |= _BV(OCIE0A);
+	attachInterrupt(0, echo, RISING);
+//   // attachInterrupt(1, left_t, RISING);
+	OCR2A = 0x0F;
+	TIMSK2 |= _BV(OCIE2A);
 }
 
-ISR(TIMER0_COMPA_vect)
+SIGNAL(TIMER2_COMPA_vect)
 {
-  TCNT0 = 0;
-  switch (stt)
-  {
-  case BEGIN:
-    {
-      digitalWrite(trigPinA, 1);
-      stt = TRIG;
-      break;
-    }
-  case TRIG:
-    {
-      digitalWrite(trigPinA, 0);
-      stt = WAIT;
-      tme = 0;
-      break;
-    }
-  case WAIT:
-    {
-      if(tme++ > 1)
-      {
-        tme = 0;
-        stt = ECHOR;
-      }
-      break;
-    }
-  case ECHOR:
-    {
-      if(digitalRead(echoPinA))
-      {
-        tme = 0;
-        stt = ECHOL;
-        digitalWrite(8,1);
-      }
-      if(tme++ > 6000)
-      {
-        tme = 0;
-        stt = ERROR1;
-      }
-      break;
-    }
-  case ECHOL:
-    {
-      if(digitalRead(echoPinA) == 0)
-      {
-        stt = BEGIN;
-        dur = tme;
-        tme = 0;
-        digitalWrite(8,0);
-      }
-      if(tme++ > 60000)
-      {
-        tme = 0;
-        stt = ERROR2;
-      }
-      break;
-    }
-  case ERROR1:
-    {
-      dur = tme + 1;
-      tme = 0;
-      stt = BEGIN;
-      err++;
-      break;
-    }
-  case ERROR2:
-    {
-      dur = tme + 2;
-      tme = 0;
-      stt = BEGIN;
-      err++;
-      break;
-    }
-  }
+	TCNT2 = 0;
+	tme++;
 }
 
-int duration, cm;
+void trig()
+{
+	digitalWrite(trigPinA, 0);
+	delayMicroseconds(5);
+	digitalWrite(trigPinA, 1);
+	delayMicroseconds(10);
+	digitalWrite(trigPinA, 0);
+}
+
+void echo()
+{
+	if (digitalRead(echoPinA))
+	{
+		tme = 0;
+		attachInterrupt(0, echo, FALLING);
+	}
+	else
+	{
+		dur = tme / 58.0;
+		attachInterrupt(0, echo, RISING);
+	}
+}
 
 void loop()
 {
-  uint16_t i = 0;
-  while(i++ < 60000);
-  Serial.print(" dur = ");
-  Serial.println(dur);
+	delay(500);
+	trig();
+	Serial.print(" dur = ");
+	Serial.print(dur);
+	Serial.println();
 }
-
