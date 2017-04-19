4cda01155dfeedd7a9d09ca0ac4b5db04a741fd3
Курушин Даниил <daniel.kurushin@gmail.com>
2017-04-19 19:48:15 +0500

    communication, moving blocked, ir receive

--- motor_ex/motor_ex.ino
+++ motor_ex/motor_ex.ino
@@ -26,86 +26,99 @@ float Kp = 10;
 uint16_t tme = 0;
 uint8_t lft_pwm = 0;
 uint8_t rgt_pwm = 0;
+uint8_t cmd = 0;
 
 float trg_spd = 100;
 
 void lft_stop()
 {
-  digitalWrite(M1AEN, LOW);
-  digitalWrite(M1BEN, LOW);
+	digitalWrite(M1AEN, LOW);
+	digitalWrite(M1BEN, LOW);
 }
 
 void rgt_stop()
 {
-  digitalWrite(M2AEN, LOW);
-  digitalWrite(M2BEN, LOW);
+	digitalWrite(M2AEN, LOW);
+	digitalWrite(M2BEN, LOW);
 }
 
 void lft_frw(float spd) //TODO ; m/s!!!
 {
-  digitalWrite(M1AEN, HIGH);
-  digitalWrite(M1AIN, HIGH);
-  digitalWrite(M1BIN, LOW);
-
-  lft_pwm = (int) max(min(spd,255),0);
-  analogWrite(M1PWM, lft_pwm);
+	// digitalWrite(M1AEN, HIGH);
+	// digitalWrite(M1AIN, HIGH);
+	// digitalWrite(M1BIN, LOW);
+	//
+	// lft_pwm = (int) max(min(spd,255),0);
+	// analogWrite(M1PWM, lft_pwm);
 }
 
 void rgt_frw(float spd)
 {
-  digitalWrite(M2AEN, HIGH);
-  digitalWrite(M2AIN, HIGH);
-  digitalWrite(M2BIN, LOW);
-
-  rgt_pwm = (int) max(min(spd,255),0);
-  analogWrite(M2PWM, rgt_pwm);
+	// digitalWrite(M2AEN, HIGH);
+	// digitalWrite(M2AIN, HIGH);
+	// digitalWrite(M2BIN, LOW);
+	//
+	// rgt_pwm = (int) max(min(spd,255),0);
+	// analogWrite(M2PWM, rgt_pwm);
 }
 
 void setup()
 {
-  Wire.begin();
-  Serial.begin(9600);
-  GY85.init();
+	Wire.begin();
+	Serial.begin(9600);
+	GY85.init();
 
-  old_z = GY85.gyro_z(GY85.readGyro());
-  for (int i=0; i < 10; i++)
+	old_z = GY85.gyro_z(GY85.readGyro());
+	for (int i=0; i < 10; i++)
 	{
-    delay(300);
-    new_z = GY85.gyro_z(GY85.readGyro());
-    delta += new_z - old_z;
-  }
-  delta /= 10;
+		delay(300);
+		new_z = GY85.gyro_z(GY85.readGyro());
+		delta += new_z - old_z;
+	}
+	delta /= 10;
 
-  pinMode(M1AIN, OUTPUT);
-  pinMode(M1BIN, OUTPUT);
-  pinMode(M1PWM, OUTPUT);
-  pinMode(M1AEN, OUTPUT);
-  pinMode(M1BEN, OUTPUT);
+	pinMode(M1AIN, OUTPUT);
+	pinMode(M1BIN, OUTPUT);
+	pinMode(M1PWM, OUTPUT);
+	pinMode(M1AEN, OUTPUT);
+	pinMode(M1BEN, OUTPUT);
 
-  pinMode(M2AIN, OUTPUT);
-  pinMode(M2BIN, OUTPUT);
-  pinMode(M2PWM, OUTPUT);
-  pinMode(M2AEN, OUTPUT);
-  pinMode(M2BEN, OUTPUT);
+	pinMode(M2AIN, OUTPUT);
+	pinMode(M2BIN, OUTPUT);
+	pinMode(M2PWM, OUTPUT);
+	pinMode(M2AEN, OUTPUT);
+	pinMode(M2BEN, OUTPUT);
+
+	pinMode(30, INPUT);
+	pinMode(31, INPUT);
+	pinMode(32, INPUT);
+}
+
+uint8_t readIRC()
+{
+	uint8_t res = (digitalRead(32) << 2) | (digitalRead(31) << 1) | (digitalRead(30));
+	return res;
 }
 
 void loop()
 {
-  pure_gz = GY85.gyro_z(GY85.readGyro());
-  gz += (pure_gz - delta) * dt;
+	pure_gz = GY85.gyro_z(GY85.readGyro());
+	gz += (pure_gz - delta) * dt;
+	cmd = readIRC();
 
-  lft_frw(trg_spd + Kp * atan(gz));
-  rgt_frw(trg_spd - Kp * atan(gz));
+	lft_frw(trg_spd + Kp * atan(gz));
+	rgt_frw(trg_spd - Kp * atan(gz));
 
-  if(++tme > 100)
-  {
-    tme = 0;
-    Serial.print("\tpure_gz "); Serial.print(pure_gz);
-    Serial.print("\tgz ");      Serial.print(gz);
-    Serial.print("\tlft_pwm "); Serial.print(lft_pwm);
-    Serial.print("\trgt_pwm "); Serial.print(rgt_pwm);
-    Serial.println();
-  }
+	if(++tme > 100)
+	{
+		tme = 0;
+		Serial.print("\tpure_gz "); Serial.print(pure_gz);
+		Serial.print("\tgz ");      Serial.print(gz);
+		Serial.print("\tlft_pwm "); Serial.print(lft_pwm);
+		Serial.print("\trgt_pwm "); Serial.print(rgt_pwm);
+		Serial.print("\tcmd ");     Serial.print(cmd);
+		Serial.println();
+	}
 
-  delay(dt * 1000);
+	delay(dt * 1000);
 }
