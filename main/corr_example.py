0d583121cc588c60de4fc223ec3b4853c95e3de2
Alexandr Larionov <alarionov93@yandex.ru>
2017-06-08 01:48:49 +0500

    2nd version of correlation example

--- main/corr_example.py
+++ main/corr_example.py
@@ -1,32 +1,24 @@
 from collections import deque
 from math import sqrt
 
+COUNT = 10
+EDGE = 0.8
+
+I = [ 1,  1,  1,  1,  1,  3,  8, 12, 14, 15, 14, 15, 16, 15, 13, 13, 11,  6,  7,  6,  3,  1,  1,  1,  1]#
 W = [ 1,  1,  1,  1,  1,  3,  6, 11, 13, 14, 14, 15, 15, 14, 14, 13, 11,  6,  7,  6,  3,  1,  1,  1,  1]
-G = [ 0,  1,  2,  3,  4,  7, 11, 17, 20, 22, 23, 25, 26, 26, 27, 27, 26, 22, 24, 24, 22, 21, 22, 23, 24]
+WS = 0
+WA = 0
+G = [ 0,  1,  2,  3,  4,  5, 11, 17, 20, 22, 23, 25, 26, 26, 27, 27, 26, 22, 24, 24, 22, 21, 22, 23, 24]
+GS = 0
+GA = 0
 M = [ 1,  1,  1,  1,  1,  2,  5, 10, 12, 13, 13, 14, 14, 13, 13, 12, 10,  5,  6,  5,  2,  0,  0,  0,  0]
-
-def avg(x=0, idx='W'):
-	# values = tmp_values[idx]
-	# values.append(x)
-	s = 0
-	for x in tmp_values[idx]:
-		s += x
-	res = s/len(tmp_values[idx])
-	
-	return res
-
-def shift(**kwargs):
-	global tmp_values
-	for idx in tmp_values.keys():
-		if len(tmp_values[idx]) > 5:
-			# tmp_values[idx].popleft()
-			tmp_values[idx] = tmp_values[idx][1:]
-		tmp_values[idx].append(kwargs[idx])
+MS = 0
+MA = 0
 
 tmp_values = {
 	'W': [],
 	'G': [],
-	'M': [],
+	'M': []
 }
 
 for t in range(len(W)):
@@ -34,33 +26,48 @@ for t in range(len(W)):
 	g = G[t]
 	m = M[t]
 
-	shift(W=w, G=g, M=m)
-	w_ = avg(w, 'W')
-	g_ = avg(g, 'G')
-	m_ = avg(m, 'M')
+	WS += w - WA
+	WA = WS / min(t+1, COUNT)
+	GS += g - GA
+	GA = GS / min(t+1, COUNT)
+	MS += m - MA
+	MA = MS / min(t+1, COUNT)
 
 	W1,W2,G1,G2,M1,M2 = [0]*6
 
+	tmp_values['W'] = ([w] + tmp_values['W'])[:COUNT]
+	tmp_values['G'] = ([g] + tmp_values['G'])[:COUNT]
+	tmp_values['M'] = ([m] + tmp_values['M'])[:COUNT]
+
 	# TODO: 1: if sqrt(0)? 2: correlation of 3 values? 3: if correlationv > 1?
 	for i in range(len(tmp_values['W'])):
-		W1+=(tmp_values['W'][i] - w_)
-		G1+=(tmp_values['G'][i] - g_)
-		M1+=(tmp_values['M'][i] - m_)
-		W2+=((tmp_values['W'][i] - w_)**2)
-		G2+=((tmp_values['G'][i] - g_)**2)
-		M2+=((tmp_values['M'][i] - m_)**2)
+		W1+=(tmp_values['W'][i] - WA)*(tmp_values['G'][i] - GA)
+		M1+=(tmp_values['G'][i] - GA)*(tmp_values['M'][i] - MA)
+		G1+=(tmp_values['M'][i] - MA)*(tmp_values['W'][i] - WA)
+		W2+=((tmp_values['W'][i] - WA)**2)
+		G2+=((tmp_values['G'][i] - GA)**2)
+		M2+=((tmp_values['M'][i] - MA)**2)
 	try:
-		r1 = W1*G1/sqrt(W2*G2)
+		r1 = W1/sqrt(W2*G2)
 	except ZeroDivisionError:
 		r1 = 'NaN'
 	try:
-		r2 = W1*M1/sqrt(W2*M2)
+		r2 = G1/sqrt(W2*M2)
 	except ZeroDivisionError:
 		r2 = 'NaN'
 	try:
-		r3 = G1*M1/sqrt(G2*M2)
+		r3 = M1/sqrt(G2*M2)
 	except ZeroDivisionError:
 		r3 = 'NaN'
+
+	course = 0
+	if r1 > EDGE:
+		course = (w + g)/2
+	elif r2 > EDGE:
+		course = (w + m)/2
+	elif r3 > EDGE:
+		course = (g + m)/2
 	
 	print('W and G: %s\t W and M: %s\t G and M: %s'%(r1, r2, r3))
+	print('Course: %s\t' % course)
 
